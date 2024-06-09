26942e68d5de26ed6563213da88f934519db355b
Lucca Tourinho <luccatourinho@gmail.com>
2024-06-09 00:40:47 -0300

    Acrescentando o Service

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,15 @@
+import {services} from "../constants/index.js";
+import ServiceCard from "../components/ServiceCard.jsx";
+
 const Services = () => {
     return (
-        <div>
-            Services
-        </div>
+        <sections
+            className={"max-container flex justify-center flex-wrap gap-9"}
+        >
+            {services.map((service) => (
+                <ServiceCard key={service.label} {...service}/>
+            ))}
+        </sections>
     )
 }
 
